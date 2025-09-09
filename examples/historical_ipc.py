a701973eac6fa318e67d86e3123b1acd762beb8f
hannahker <hannah.ker96@gmail.com>
2025-09-08 17:33:40 -0700

    remove src

--- examples/historical_ipc.py
+++ examples/historical_ipc.py
@@ -68,8 +68,11 @@ def _(mo):
 def _():
     import pandas as pd
     import plotly.express as px
+    import requests 
+    import os
+    from dotenv import load_dotenv
 
-    from src.datasources.ipc import get_ipc_from_hapi, combine_4_plus
+    load_dotenv()
 
     iso3s = {
         'Angola': 'AGO',
@@ -90,7 +93,62 @@ def _():
     }
 
     severity_levels = ["3+", "4+"]
-    return combine_4_plus, get_ipc_from_hapi, iso3s, pd, px, severity_levels
+    return iso3s, os, pd, px, requests, severity_levels
+
+
+@app.cell
+def _(os, pd, requests):
+    def get_ipc_from_hapi(iso3=None):
+        endpoint = (
+            "https://hapi.humdata.org/api/v2/food-security-nutrition-poverty/food-security"
+        )
+        params = {
+            "app_identifier": os.getenv("HAPI_APP_IDENTIFIER"),
+            "admin_level": 0,
+            "output_format": "json",
+            "limit": 10000,
+            "offset": 0,
+        }
+        if iso3:
+            params["location_code"] = iso3
+        # Check if the request was successful
+        response = requests.get(endpoint, params=params)
+        json_data = response.json()
+        # Extract the data list from the JSON
+        data_list = json_data.get("data", [])
+        df_response = pd.DataFrame(data_list)
+
+        if df_response.empty:
+            raise Exception(f"No data available for {iso3}")
+        df_response["From"] = pd.to_datetime(df_response["reference_period_start"])
+        df_response["To"] = pd.to_datetime(df_response["reference_period_end"])
+        df_response["year"] = df_response["To"].dt.year
+        return df_response.sort_values("reference_period_start", ascending=False)[
+            [
+                "location_code",
+                "ipc_phase",
+                "ipc_type",
+                "population_in_phase",
+                "population_fraction_in_phase",
+                "From",
+                "To",
+                "year",
+            ]
+        ]
+
+
+    def combine_4_plus(df_all):
+        df = df_all.copy()
+        mapping = {"4": "4+", "5": "4+"}
+        df["ipc_phase"] = df["ipc_phase"].map(mapping).fillna(df["ipc_phase"])
+        dff = df[df.ipc_phase == "4+"]
+        dff = (
+            dff.groupby(["From", "To", "location_code", "ipc_type", "year", "ipc_phase"])
+            .agg({"population_in_phase": "sum", "population_fraction_in_phase": "sum"})
+            .reset_index()
+        )
+        return pd.concat([df_all, dff])
+    return combine_4_plus, get_ipc_from_hapi
 
 
 @app.cell
